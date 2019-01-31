1a16b800ccad3efda4a3e4c56334261c76c6d32a
Cristian <cristian.holdunu@gmail.com>
2019-01-31 22:40:47 +0200

    add method for hiding the keyboard

--- keyboardheightprovider/src/main/java/com/hold1/keyboardheightprovider/KeyboardHeightProvider.kt
+++ keyboardheightprovider/src/main/java/com/hold1/keyboardheightprovider/KeyboardHeightProvider.kt
@@ -1,12 +1,14 @@
 package com.hold1.keyboardheightprovider
 
 import android.app.Activity
+import android.content.Context
 import android.graphics.Point
 import android.graphics.Rect
 import android.view.Gravity
 import android.view.View
 import android.view.ViewTreeObserver
 import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
 import android.widget.PopupWindow
 
 /**
@@ -109,6 +111,11 @@ class KeyboardHeightProvider(val activity: Activity) : PopupWindow(activity) {
     fun close() {
         this.keyboardListeners.clear()
         dismiss()
+    }
+
+    fun hideKeyboard() {
+        val imm = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(parentView.windowToken, 0)
     }
 
     interface KeyboardListener {
